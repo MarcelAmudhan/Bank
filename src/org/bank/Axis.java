cc502cd507e58b1f3eab716aaad9523afbc0c451
MarcelAmudhan <marcelamudhan@gmail.com>
2021-09-15 13:03:47 +0530

    conflict Resolved

--- src/org/bank/Axis.java
+++ src/org/bank/Axis.java
@@ -2,7 +2,6 @@ package org.bank;
 
 public class Axis extends BankData {
 	private void accountNo() {
-<<<<<<< HEAD
 		
 System.out.println("23456567788");
 	}
@@ -13,18 +12,6 @@ System.out.println("Sumathi");
 private void ifsc() {
 	
 System.out.println("2222");
-=======
-
-System.out.println("23456567788");
-	}
-	private void acHolderName() {
-
-		System.out.println("Ajith Prasanna");
-	}
-private void ifsc() {
-	
-System.out.println("0122");
->>>>>>> d7624f9dcc9cac4a6fe9b56d8da9550ebdb6c006
 }
 public static void main(String[] args) {
 	Axis a=new Axis();
