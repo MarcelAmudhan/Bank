43b276ca8c829ffa4c80ba18d5eb79063e4ea29f
MarcelAmudhan <marcelamudhan@gmail.com>
2021-09-15 12:47:42 +0530

    Code updated by Sumathi

--- src/org/bank/Axis.java
+++ src/org/bank/Axis.java
@@ -2,11 +2,15 @@ package org.bank;
 
 public class Axis extends BankData {
 	private void accountNo() {
-		// TODO Auto-generated method stub
+		
 System.out.println("23456567788");
 	}
+	private void holderName() {
+		
+System.out.println("Sumathi");
+	}
 private void ifsc() {
-	// TODO Auto-generated method stub
+	
 System.out.println("2222");
 }
 public static void main(String[] args) {
